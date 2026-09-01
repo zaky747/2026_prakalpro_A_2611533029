d6d7d0c94c3d1a14569f74d665eee10956c4b0da
zaky747 <zakyafiqtaha@gmail.com>
2026-09-01 10:07:12 +0700

    pro

---
+++ program1.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\")
+print("program ini" + " dibuat oleh a/n"
++ " zaky afiq taha dan 2611533029.")
